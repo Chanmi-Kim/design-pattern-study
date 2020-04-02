142e78a21066dec518c9cc4f85bb15547734343f
Chanmi-Kim <chanmi_dev@naver.com>
2020-04-02 16:22:59 +0900

    feat: Add class AdapterPhoneByUSBport in Adapter Pattern

---
+++ Adapter-Pattern/AdapterPhoneByUSBport.java
@@ -0,0 +1,14 @@
+package Adapter;
+
+public class AdapterPhoneByUSBport extends Phone implements Connectable {
+
+    @Override
+    public void connectSamsungPhone() {
+        connectSamsungPhone();
+    }
+
+    @Override
+    public void connectLGPhone() {
+        connectLGPhone();
+    }
+}
