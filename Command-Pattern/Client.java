421b93a304bd11a02e3a3b1119f808ecac4c4127
Chanmi-Kim <chanmi_dev@naver.com>
2020-04-02 16:20:26 +0900

    feat: Add class Client in Command Pattern

---
+++ Command-Pattern/Client.java
@@ -0,0 +1,16 @@
+public class Client {
+    public static void main(String args[]) {
+        Heater heater = new Heater();
+        Lamp lamp = new Lamp();
+
+        Command heaterOnCommand = new HeaterOnCommand(heater);
+        Command lampOnCommand = new LampOnCommand(lamp);
+        OKGoogle okGoogle = new OKGoogle();
+
+        okGoogle.setCommand(heaterOnCommand);
+        okGoogle.talk();
+
+        okGoogle.setCommand(lampOnCommand);
+        okGoogle.talk();
+    }
+}
