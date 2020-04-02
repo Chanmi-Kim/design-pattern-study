c7c4e575701e616d32dd2f1e07831d2883966fd6
Chanmi-Kim <chanmi_dev@naver.com>
2020-04-02 16:22:37 +0900

    feat: Add class Phone in Adapter Pattern

---
+++ Adapter-Pattern/Phone.java
@@ -0,0 +1,13 @@
+package Adapter;
+
+public class Phone {
+
+    void connectSamsungPhone() {
+        System.out.println("connectSamsungPhone");
+    }
+
+    void connectLGPhone() {
+        System.out.println("connectLGPhone");
+    }
+
+}
